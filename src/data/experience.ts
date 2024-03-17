d0e86e6dd7e60a3221e5ef6ad4a91301d32299e2
Alejo <alejoczombos@gmail.com>
2024-03-17 18:53:33 -0300

    actualizacion experiencia

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -11,7 +11,7 @@ export const experience: Experience[] = [
     title: "Desarrollador Full Stack - Pasante",
     company: "Club de la Universidad del Nordeste",
     date_start: "Diciembre 2023",
-    date_end: "actualidad",
+    date_end: "Febrero 2024",
     description: [
       "Líder del desarrollo del Sistema de Gestión Empresarial realizado con una API REST creada con Spring Boot y Java, React con JavaScript y MySQL.",
       "Realicé con éxito la migración de la Base de Datos desde Access 95 a MySQL, mejorando significativamente la eficiencia y escalabilidad del sistema.",
