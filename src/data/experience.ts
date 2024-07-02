630d0c787fb16af08b243d4457801408bd9b9df1
Alejo <alejoczombos@gmail.com>
2024-07-01 22:40:00 -0300

    experiencia

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -8,14 +8,23 @@ export type Experience = {
 
 export const experience: Experience[] = [
   {
+    title: "Desarrollador Full Stack",
+    company: "Vida Digital S.A.S.",
+    date_start: "Abril 2024",
+    date_end: "Julio 2024",
+    description: [
+      "Desarrollé con éxito la aplicación “Smart C2” que se encarga de la trazabilidad de la cadena de frío de los medicamentos.",
+      "Trabajé durante todo el proceso con las tecnologías Vue.js, Express.js, Firebase Firestore.",
+    ],
+  },
+  {
     title: "Desarrollador Full Stack - Pasante",
     company: "Club de la Universidad del Nordeste",
     date_start: "Diciembre 2023",
     date_end: "Febrero 2024",
     description: [
-      "Líder del desarrollo del Sistema de Gestión Empresarial realizado con una API REST creada con Spring Boot y Java, React con JavaScript y MySQL.",
-      "Realicé con éxito la migración de la Base de Datos desde Access 95 a MySQL, mejorando significativamente la eficiencia y escalabilidad del sistema.",
-      "Colaboré estrechamente con equipos interdisciplinarios para asegurar una implementación fluida y una experiencia de usuario excepcional."
+      "Lideré el desarrollo del Sistema de Gestión Empresarial realizado con una API REST creada con Spring Boot MySQL y React.",
+      "Realicé con éxito la migración de la Base de Datos desde Access 95 a MySQL, mejorando significativamente la eficiencia y escalabilidad del sistema",
     ],
   },
 ];
