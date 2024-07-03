7ab1638e409118c2bfe26c362d6b67a43fde6ac3
Alejo <alejoczombos@gmail.com>
2024-07-02 21:17:36 -0300

    Tecnologias y actualizar CV

--- src/data/techs.ts
+++ src/data/techs.ts
@@ -1,18 +1,17 @@
 import { TechIcons } from "@data/projects.ts";
 
 export const techs: TechIcons[] = [
-    TechIcons.Spring,
-    TechIcons.Java,
-    TechIcons.Hibernate,
-    TechIcons.React,
-    TechIcons.TypeScript,
-    TechIcons.Git,
-    TechIcons.Docker,
-    TechIcons.Swagger,
-    TechIcons.JUnit,
-    TechIcons.TailwindCSS,
-    TechIcons.JWT,
-    TechIcons.MySQL,
-    TechIcons.PostgreSQL,
-    TechIcons.SQLServer,
-]
+  TechIcons.Spring,
+  TechIcons.Java,
+  TechIcons.Hibernate,
+  TechIcons.React,
+  TechIcons.TypeScript,
+  TechIcons.Git,
+  TechIcons.Docker,
+  TechIcons.Swagger,
+  TechIcons.JUnit,
+  TechIcons.JWT,
+  TechIcons.MySQL,
+  TechIcons.PostgreSQL,
+  TechIcons.SQLServer,
+];
