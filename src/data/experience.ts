e76eff8dba616d481457e3cd11e7ae5822bf3ea2
Alejo <alejoczombos@gmail.com>
2024-12-19 18:51:08 -0300

    experiencia

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -9,13 +9,23 @@ export type Experience = {
 export const experience: Experience[] = [
   {
     title: "Desarrollador Full Stack",
-    company: "Freelancer",
-    date_start: "Mayo 2024",
+    company: "EME (Proyecto Freelance)",
+    date_start: "Noviembre 2024",
     date_end: "Presente",
     description: [
-      "Diseño, desarrollo y despliegue de aplicaciones y sitios web personalizados para empresas medianas y pequeñas utilizando HTML, CSS, JavaScript y React.",
-      "Desarrollo de CMS personalizados utilizando Python (FastAPI) y MySQL, además de integrar soluciones con otras plataformas empresariales mediante Python.",
-      "Capacidad de análisis y resolución de problemas en desarrollo web, logrando soluciones eficientes y prácticas.",
+      "Diseño, desarrollo y despliegue de sitio web personalizado utilizando HTML, CSS, JavaScript, React y Astro.",
+      "Desarrollo de CMS personalizados utilizando Python (FastAPI) y MySQL.",
+      "Resolución de problemas en desarrollo web, implementando soluciones eficientes y prácticas.",
+    ],
+  },
+  {
+    title: "Desarrollador Frontend",
+    company: "Connectto (Proyecto Freelance) ",
+    date_start: "Agosto 2024",
+    date_end: "Septiembre 2024",
+    description: [
+      "Desarrollo y despliegue de sitio web personalizado utilizando HTML, CSS, JavaScript y Astro.",
+      "Colaboración con el equipo de diseño UX para asegurar coherencia visual y funcionalidad.",
     ],
   },
   {
@@ -28,17 +38,17 @@ export const experience: Experience[] = [
       "Análisis, diseño y desarrollo de APIs REST utilizando Node.js y Express.js, siguiendo principios de arquitectura modular.",
       "Diseño y gestión de bases de datos NoSQL con Firebase Firestore",
       "Implementación de un servicio de autenticación y autorización usando Firebase Authentication, fortaleciendo el control de acceso y seguridad de la aplicación.",
-      "Control de versiones con GitLab."
+      "Control de versiones con GitLab.",
     ],
   },
   {
-    title: "Desarrollador Full Stack - Pasante",
+    title: "Desarrollador Full Stack",
     company: "Club de la Universidad del Nordeste",
     date_start: "Diciembre 2023",
     date_end: "Febrero 2024",
     description: [
       "Diseño, desarrollo y despliegue de un Sistema de Gestión Empresarial Interna para optimizar la administración de recursos y operaciones.",
-      "Análisis, diseño y desarrollo de una API RESTful utilizando Spring Boot y Java, integrando Hibernate y JDBC para el manejo eficiente de datos y utilizando Spring Security para robustecer la seguridad del sistema.",
+      "Análisis, diseño y desarrollo de una API RESTful utilizando Spring Boot y Java, integrando Hibernate y JDBC para el manejo eficiente de datos y utilizando Spring Security para robustecer la seguridaddel sistema.",
       "Diseño y gestión de bases de datos relacionales con MySQL, migrando exitosamente desde Access 95 y mejorando la escalabilidad y rendimiento de la base de datos.",
       "Desarrollo de un sistema de autenticación y autorización basado en JWT, permitiendo la gestión de roles y permisos de acceso de forma segura y eficaz.",
     ],
